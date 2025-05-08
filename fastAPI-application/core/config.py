10d55ee09b9c9b20898f740b03a87f24b57c6c73
Fiskoff <fiskoff2015@yandex.ru>
2025-05-08 14:41:59 +0700

    Create database config

--- fastAPI-application/core/config.py
+++ fastAPI-application/core/config.py
@@ -11,9 +11,17 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class BaseConfig(BaseModel):
+    url: str = "sqlite+aiosqlite://messenger.sqlite3"
+    echo: bool = True
+    pool_size: int = 10
+    max_overflow: int = 15
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: BaseConfig
 
 
 settings = Settings()
